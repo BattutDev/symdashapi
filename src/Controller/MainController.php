a13998f00f52c0be4bc52489cdffc82fb9f021b3
BattutDev <antoine@battut.dev>
2023-08-13 20:13:58 +0200

    fix: quick fixes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/")
+ */
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main', methods: ['GET'])]
