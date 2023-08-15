2df65ec09f53b214635a5d3b820f3ad41d16c67b
BattutDev <antoine@battut.dev>
2023-08-15 11:05:49 +0200

    fix: return json response for http error code

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,10 +2,12 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ExceptionListener
 {
@@ -13,6 +15,7 @@ class ExceptionListener
     {
         $exception = $event->getThrowable();
 
+        /*
         if ($exception instanceof AccessDeniedHttpException) {
             // Créez une réponse personnalisée
             $response = new JsonResponse([
@@ -22,5 +25,32 @@ class ExceptionListener
 
             $event->setResponse($response);
         }
+        else
+        if ($exception instanceof NotFoundHttpException) {
+            // Créez une réponse personnalisée
+            $response = new JsonResponse([
+                'code' => 404,
+                'message' => 'Route not found'
+            ], Response::HTTP_NOT_FOUND);
+
+            $event->setResponse($response);
+        }
+        else
+        if ($exception instanceof InternalErrorException) {
+            // Créez une réponse personnalisée
+            $response = new JsonResponse([
+                'code' => 500,
+                'message' => 'Internal Server Error'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+
+            $event->setResponse($response);
+        }
+        */
+        $event->setResponse(new JsonResponse([
+            'code' => $exception->getStatusCode(),
+            'message' => $exception->getMessage()
+        ]));
+
+
     }
 }
